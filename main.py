7127572779be489ac4ecfa118082a68322be917e
binvij <bindeshvijayan@gmail.com>
2024-06-01 01:18:17 +0400

    initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+import requests 
+import streamlit as st
+
+
+
+with st.sidebar:
+	st.header("About SkyChat")
+	st.markdown(
+        """
+        **SkyChat** is an **AI Enabled Assistant**, which can help answers your doubts/queries relating to skydiving principles and fundamentals. 
+        """
+		)
+
+
+st.title("SkyChat")
+
+
+#initiliaze chat history 
+if "messages" not in st.session_state:
+	st.session_state.messages = []
+
+# display chat message from history on app rerun
+for message in st.session_state.messages:
+	with st.chat_message(message["role"]):
+		st.markdown(message["content"])
+
+
+# react to user input 
+if prompt := st.chat_input("what do you want to know?"):
+	# display user message in chat message container 
+	with st.chat_message("user"):
+		st.markdown(prompt)
+
+	# add user message to chat history
+	st.session_state.messages.append({"role": "user", "content": prompt})
+	data = {"name": "user", "query": prompt}
+
+	with st.spinner("searching for answer..."):
+		resp = requests.post("https://skybotapi.azurewebsites.net/bot/v1/ask", json= data)
+		output_text = ""
+		if resp.status_code == 200:
+			output_text = resp.json()["response"]
+		else:
+			output_text = "An error occurred processing your message. Please try again later"
+			st.chat_message("assistant").markdown(output)
+
+
+		with st.chat_message("assistant"):
+			st.markdown(output_text)
+			st.session_state.messages.append({"role":"assistant", "content": output_text})
+
+
+
